e6043ea57e1f29658e8a898db03822b84433622f
Cade Kynaston <cade.kynaston@clearlink.com>
2019-12-28 15:34:48 -0700

    deleted dummy data

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,5 @@
-import dummyUsers from './dummydata';
 import createGraphData from './createGraphData';
 
-export { dummyUsers };
 export { createGraphData };
 export { getGithubUser } from './dataFetching';
 export { getGithubUserRepos } from './dataFetching';
