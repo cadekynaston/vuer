5eea84c24fc403ff2f2554bd6288153aec8dded4
Cade Kynaston <cade.kynaston@clearlink.com>
2019-12-30 12:25:04 -0700

    removed vue-meta package

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
-import VueMeta from 'vue-meta';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faChartPie, faUserCircle, faUser, faExternalLinkAlt, faSearch, faSort, faCalendarPlus, faMapMarkerAlt, faUserFriends,
 } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-import metaInfo from './shared/meta';
 import './registerServiceWorker';
 
 import App from './App.vue';
@@ -22,10 +19,8 @@ Vue.config.productionTip = false;
 Vue.use(VueGtag, {
   config: { id: 'UA-137604730-6' },
 });
-Vue.use(VueMeta);
 
 new Vue({
   store,
-  metaInfo,
   render: h => h(App),
 }).$mount('#app');
